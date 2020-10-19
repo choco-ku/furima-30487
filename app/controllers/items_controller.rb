80e5f1c0d8c6dd0e9a46b404db4fa6e8cc7e7938
choco-ku <2choco37@gmail.com>
2020-10-19 19:57:30 +0900

    商品情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,10 @@
 class ItemsController < ApplicationController
 
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :move_to_index, except: [:index, :show]
 
  def index
-   @items = Item.all.order("created_at DESC")
+   @items = Item.includes(:user).order("created_at DESC")
  end
   
  def new 
@@ -23,17 +24,32 @@ class ItemsController < ApplicationController
    @item = Item.find(params[:id])
  end
 
+ def edit
+   @item = Item.find(params[:id])
+   if current_user.id != @item.user_id
+    redirect_to root_path
+   end
+ end
+
+ def update
+    item = Item.find(params[:id])
+    if item.update(item_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+ end
+
+ def move_to_index
+  unless user_signed_in?
+    redirect_to action: :index
+  end
+ end
+
 
   private
   def item_params
     params.require(:item).permit(:name, :content, :image, :category_id, :condition_id, :shipping_fee_id, :shipping_place_id, :shipping_date_id, :price).merge(user_id: current_user.id)
-  end
-
-
-  def move_to_index
-    unless user_signed_in?
-      redirect_to action: :index
-    end
-  end
+  end 
   
 end 
